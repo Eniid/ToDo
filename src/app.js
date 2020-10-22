cb2d026625be298245b6e3492a5d518fac8056ca
Enid <enid@Cordelia.local>
2020-10-22 13:46:53 +0200

    checkbox apearance fixed

--- src/app.js
+++ src/app.js
@@ -3,8 +3,46 @@ import 'alpinejs'
 console.log('test');
 window.data = function() {
     return{
-        display: false, 
-        addTodo(){}, 
-        deleteTodo(){},
+        filter : 'all',
+        todos: [],
+        newTodoTitle: null, 
+        get activeTodos (){
+            return this.todos.filter(todo => todo.completed !== true); 
+        },
+        get activeTodosCounts (){
+            return this.activeTodos.length
+        },
+        get completedTodos (){
+            return this.todos.filter(todo => todo.completed === true); 
+        },
+        get completedTodosCounts (){
+            return this.completedTodos.length
+        },
+        get filtedTodo(){
+            return {
+                all: this.todos, 
+                active:this.activeTodos, 
+                completed: this.completedTodos,
+            }[this.filter]
+        },
+
+
+        addTodo(){
+            if(this.newTodoTitle.trim()){
+                this.todos.push({
+                    id:Date.now(), 
+                    title: this.newTodoTitle, 
+                    completed: false, 
+                })   
+            }
+            this.newTodoTitle = '';
+        },
+        removeTodo (todo) {
+            this.todos.splice(this.todos.indexOf(todo),1)
+        },
+        toggleCompleted(todo) {
+            todo.completed = !todo.completed
+        },
+
     }
 }
